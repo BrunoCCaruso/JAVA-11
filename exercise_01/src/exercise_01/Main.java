df37890e860f083034821cc649f222f560a10b38
Bruno Cesar Caruso <brunoccaruso.s@gmail.com>
2022-03-11 20:17:55 -0300

    First commit

---
+++ exercise_01/src/exercise_01/Main.java
@@ -0,0 +1,15 @@
+/////////////////// exercise 01 ///////////////////////////////////////////
+/*
+*
+* 
+*/
+
+
+
+package exercise_01;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
