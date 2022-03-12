7e1cbcbdcfbf6bbeea9421533d14485a517c7ea2
Bruno Cesar Caruso <brunoccaruso.s@gmail.com>
2022-03-11 21:00:26 -0300

    Exercise 01 finalized

--- exercise_01/src/exercise_01/Main.java
+++ exercise_01/src/exercise_01/Main.java
@@ -1,15 +1,31 @@
 /////////////////// exercise 01 ///////////////////////////////////////////
 /*
-*
-* 
+Initialize an object of the class Person, to null and try to
+invoke the isOlderAge() method through that object. Then wrap the code
+with a try-catch clause to test the new exception to be handled.
 */
-
-
 
 package exercise_01;
 
 public class Main {
     public static void main(String[] args) {
+
+        Person myPerson = new Person();
+        myPerson = null;   // <----- The error is here.
+        try{
+            myPerson.isOlderAge();
+        }catch(NullPointerException e){
+            System.err.println("The following error occurs: " + e.getClass().getSimpleName());
+            System.out.println("This error can appear when an objet is not initialized.");
+        }finally {
+            System.out.println("Finalized.");
+        }
+
+
+
+
+
+
 
     }
 }
---
+++ exercise_01/src/exercise_01/Person.java
@@ -0,0 +1,16 @@
+package exercise_01;
+
+public class Person {
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public void isOlderAge(){
+        System.out.println("Is older.");
+    }
+}
